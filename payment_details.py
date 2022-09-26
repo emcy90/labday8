a25cbc2ec7ebffb3a1883fb9796b38a7042fb2d1
abshir <abshir_4@hotmail.com>
2022-09-26 14:23:20 +0200

    payment_details

--- payment_details.py
+++ payment_details.py
@@ -1,8 +1,11 @@
+from typing import Optional
+from dataclasses import dataclass
 """
 payment-details
 """
 
-class Payment_detail:
+
+class PaymentDetail:
     """python class payment-details that stores data about payments."""
 
     def __init__(self, payment_method, transition_id, amount, payment_status, cardnumber):
@@ -18,3 +21,29 @@ class Payment_detail:
     def short_string(self) -> str:
         """function to return payment_method and payment_status as string"""
         return f"payment method and payment status {self.payment_method}, {self.payment_status}"
+
+    def pay_method(self) -> str:
+        """Returns which payment method"""
+        return self.payment_method
+
+    def pay_status(self) -> str:
+        """Returns which payment status"""
+        return self.payment_status
+
+
+@dataclass
+class Payment:
+    payment_status: Optional[PaymentDetail] = None
+    payment_method: Optional[PaymentDetail] = None
+
+    def payment_stat(self) -> str:
+        """payment status."""
+        witch_payment_status = self.payment_status.pay_status()
+        if self.payment_status is not None:
+            return witch_payment_status
+
+    def payment_met(self) -> str:
+        """payment method."""
+        witch_payment_method = self.payment_method.pay_method()
+        if self.payment_method is not None:
+            return witch_payment_method
