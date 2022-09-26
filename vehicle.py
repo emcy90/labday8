364dd5d7c47f7b96a5bc5258250bd39dbfb7a1a8
abshir <abshir_4@hotmail.com>
2022-09-26 14:02:49 +0200

    vehicle ändrar

--- vehicle.py
+++ vehicle.py
@@ -1,17 +1,19 @@
+from typing import Optional
+from dataclasses import dataclass
 """
 vehicle
 """
 
 
 class Vehicle:
-    """python class location that stores data."""
+    """python class vehicle stores data."""
 
     def __init__(self, vehicle_no, bike, truck, capacity, current_status):
         self.vehicle_no = vehicle_no
         self.truck = truck
         self.bike = bike
         self.capacity = capacity
-        self. current_status = current_status "[Free,busy,Not_working]"
+        self. current_status = current_status
 
     def __str__(self):
         return self.short_string()
@@ -19,3 +21,20 @@ class Vehicle:
     def short_string(self) -> str:
         """function to return vehicle as string"""
         return f"bike and truck {self.bike}, {self.truck}"
+
+    def get_status(self) -> str:
+        """Returns which current status"""
+        return self.current_status
+
+
+@dataclass
+class Status:
+    current_status: Optional[Vehicle] = None
+
+    def status(self) -> str:
+        """which person is specific course."""
+        witch_status = self.current_status.get_status()
+        if self.current_status is not None:
+            return witch_status
+
+
