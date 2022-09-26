e5af7e13a42fd3270250d9fb9255f944ca4e2340
Emcy90 <emma@emcy.se>
2022-09-26 13:14:55 +0200

    base info for location

---
+++ location.py
@@ -0,0 +1,17 @@
+"""
+location
+"""
+
+class Location:
+    """python class location that stores data."""
+
+    def __init__(self, longitude, latitude):
+        self.longitude = longitude
+        self.latitude = latitude
+
+    def __str__(self):
+        return self.short_string()
+
+    def short_string(self) -> str:
+        """function to return longitude and latitude as string"""
+        return f"Longitude and latitude {self.longitude}, {self.latitude}"
